16945a9cf28ceec7a9f9cfb6a3c30ea6a4ac36a9
Aman Agnihotri <awfully.awesome@yahoo.com>
2015-03-24 17:41:42 +0530

    Add SignalContext
    In StrangeDemonstration, the examples utilise Signals for
    communication. The SignalContext makes sure that Signals become the
    default way of communication instead of Events.
    
    Signals provide type safety over Events in StrangeIoC, hence their use
    in the project.

---
+++ Assets/Scripts/Common/Config/SignalContext.cs
@@ -0,0 +1,21 @@
+﻿using strange.extensions.command.api;
+using strange.extensions.command.impl;
+using strange.extensions.context.impl;
+
+namespace Demonstration.Common
+{
+  public class SignalContext : MVCSContext
+  {
+    public SignalContext (ContextView contextView) : base (contextView)
+    {
+    }
+
+    protected override void addCoreComponents ()
+    {
+      base.addCoreComponents ();
+
+      injectionBinder.Unbind<ICommandBinder> ();
+      injectionBinder.Bind<ICommandBinder> ().To<SignalCommandBinder> ().ToSingleton ();
+    }
+  }
+}
