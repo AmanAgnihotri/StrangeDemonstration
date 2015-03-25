c7905b6508849c83a39a22652eabf5000ff7d77d
Aman Agnihotri <awfully.awesome@yahoo.com>
2015-03-25 22:40:31 +0530

    Add IConcurrentQueue & ConcurrentQueue
    This generic implementation helps in dealing with task polling when
    multiple threads are involved.

---
+++ Assets/Scripts/Common/Interface/IConcurrentQueue.cs
@@ -0,0 +1,15 @@
+﻿namespace Demonstration.Common
+{
+  public interface IConcurrentQueue<T>
+  {
+    int Count { get; }
+
+    void Enqueue (T item);
+
+    T Dequeue () ;
+
+    T Peek ();
+
+    void Clear ();
+  }
+}
---
+++ Assets/Scripts/Common/Model/ConcurrentQueue.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace Demonstration.Common
+{
+  internal class ConcurrentQueue<T> : IConcurrentQueue<T>
+  {
+    private readonly object syncLock = new object ();
+
+    private Queue<T> Queue { get; set; }
+
+    public int Count
+    {
+      get
+      {
+        lock (syncLock)
+        {
+          return Queue.Count;
+        }
+      }
+    }
+
+    public ConcurrentQueue ()
+    {
+      Queue = new Queue<T> ();
+    }
+
+    public void Enqueue (T obj)
+    {
+      lock (syncLock)
+      {
+        Queue.Enqueue (obj);
+      }
+    }
+
+    public T Dequeue ()
+    {
+      lock (syncLock)
+      {
+        return Queue.Dequeue ();
+      }
+    }
+
+    public T Peek ()
+    {
+      lock (syncLock)
+      {
+        return Queue.Peek ();
+      }
+    }
+
+    public void Clear ()
+    {
+      lock (syncLock)
+      {
+        Queue.Clear ();
+      }
+    }
+  }
+}
