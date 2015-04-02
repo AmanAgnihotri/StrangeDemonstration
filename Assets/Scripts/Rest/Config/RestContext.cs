82909b346a405ad82b5188953df89b5620b08e92
Aman Agnihotri <awfully.awesome@yahoo.com>
2015-04-02 23:07:17 +0530

    Add Rest Bootstrap and Context
    This is the starting point for the Rest module. This module will use
    the RestSharp library to communicate with a REST API based service.

---
+++ Assets/Scripts/Rest/Config/RestContext.cs
@@ -0,0 +1,19 @@
+﻿using Demonstration.Common;
+using strange.extensions.context.impl;
+
+namespace Demonstration.Rest
+{
+  public class RestContext : SignalContext
+  {
+    public RestContext (ContextView contextView) : base (contextView)
+    {
+    }
+
+    protected override void mapBindings ()
+    {
+      base.mapBindings ();
+
+
+    }
+  }
+}
---
+++ Assets/Scripts/Rest/RestBootstrap.cs
@@ -0,0 +1,12 @@
+﻿using strange.extensions.context.impl;
+
+namespace Demonstration.Rest
+{
+  public class RestBootstrap : ContextView
+  {
+    private void Awake ()
+    {
+      context = new RestContext (this);
+    }
+  }
+}
